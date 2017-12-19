31952ad560a04504bab53cb8aa05f0f0ad12a93f
Thomas R. Benson <benson31@llnl.gov>
2017-12-19 13:38:45 -0800

    add some missing files

---
+++ include/El/Meta.hpp
@@ -0,0 +1,13 @@
+#ifndef EL_META_HPP_
+#define EL_META_HPP_
+
+namespace El
+{
+
+template<typename Condition,class T=void>
+using EnableIf = typename std::enable_if<Condition::value,T>::type;
+template<typename Condition,class T=void>
+using DisableIf = typename std::enable_if<!Condition::value,T>::type;
+
+}//namespace El
+#endif // EL_META_HPP_
---
+++ include/El/Types/InertiaType.hpp
@@ -0,0 +1,15 @@
+#ifndef EL_TYPES_INERTIATYPE_HPP_
+#define EL_TYPES_INERTIATYPE_HPP_
+
+#include "El/Typedefs.hpp"
+
+namespace El
+{
+
+struct InertiaType
+{
+    Int numPositive, numNegative, numZero;
+};
+
+}// namespace El
+#endif // EL_TYPES_INERTIATYPE_HPP_
